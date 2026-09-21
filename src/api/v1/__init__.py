c97ac9f752e6f998f859224d89ac23a40ea063b0
AndreyKosov <andrey.kosov.05@inbox.ru>
2026-09-22 00:13:07 +0500

    22.09.2026 register media endpoint

--- src/api/v1/__init__.py
+++ src/api/v1/__init__.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter
 
-from . import assets, collections, users
+from . import assets, collections, media, users
 
 router = APIRouter(prefix="/v1")
 
 router.include_router(assets.router)
 router.include_router(collections.router)
+router.include_router(media.router)
 router.include_router(users.router)
 
 __all__ = ["router"]
